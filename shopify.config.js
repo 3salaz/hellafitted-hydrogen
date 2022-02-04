c75a7fa16e8c5905b66e5624058f6cda7fa7e10c
E <work@Es-MacBook-Pro.local>
2022-02-03 19:53:53 -0800

    initial commit

---
+++ shopify.config.js
@@ -0,0 +1,6 @@
+export default {
+  defaultLocale: 'en-us',
+  storeDomain: 'hellafitted.com',
+  storefrontToken: 'b240b7aa31a9366165c5912d7b0deb6c',
+  storefrontApiVersion: 'unstable',
+};
---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    // screens: {
+    //   sm: '480px',
+    //   md: '768px',
+    //   lg: '976px',
+    //   xl: '1440px',
+    // },
+    // colors: {
+    //   'blue': '#1fb6ff',
+    //   'pink': '#ff49db',
+    //   'orange': '#ff7849',
+    //   'green': '#13ce66',
+    //   'gray-dark': '#273444',
+    //   'gray': '#8492a6',
+    //   'gray-light': '#d3dce6',
+    // },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            hr: {
+              borderColor: theme('colors.gray.200'),
+              borderTopWidth: '1px',
+              marginTop: '2rem',
+              marginBottom: '2rem',
+            },
+            'ol > li::before': {
+              color: theme('colors.gray.900'),
+            },
+            'ul > li::before': {
+              backgroundColor: theme('colors.gray.900'),
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
